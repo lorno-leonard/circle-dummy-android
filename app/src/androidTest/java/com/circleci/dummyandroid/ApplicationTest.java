f89b1a8101afdd7cb5b469ec6a701b8f31952dc0
Leonard Laput <online.lorno@gmail.com>
2015-07-13 16:52:30 +0800

    test(num_equality): number equality test

--- app/src/androidTest/java/com/circleci/dummyandroid/ApplicationTest.java
+++ app/src/androidTest/java/com/circleci/dummyandroid/ApplicationTest.java
@@ -2,6 +2,7 @@ package com.circleci.dummyandroid;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +10,10 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @SmallTest
+    public void testNumEquality() {
+        assertTrue("Test => 1 == 2", 1 == 2);
     }
 }
